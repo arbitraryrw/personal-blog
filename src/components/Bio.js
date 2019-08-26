2dd82ca5dd95ce0ea1e70dcb47675dc4e44db251
Nikola <nickcucakovic@gmail.com>
2019-08-26 20:52:16 +0100

    Adding avatar and removing gravatar logic

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import styles from './Bio.module.css'
-import { getGravatarURL } from '../utils/getGravatarURL'
+import avatar from '../imgs/avatar.jpg'
 
 function Bio(props) {
-  let photoURL = getGravatarURL({
-    email: "test1@example.com",
-    size: 56,
-  })
 
   return (
     <div className={`
       ${styles.Bio}
       ${props.className || ''}
     `}>
-      <img src={photoURL} alt="Me" />
+      <img src={avatar} alt="Me" />
       <p>
+
         Create a blog with a single command, by{' '}
         <a href="https://twitter.com/james_k_nelson/">James K Nelson</a>.
         <br />
