39b490cb13e9dcde9d6c5114d532cd4c181563bd
Nikola <nickcucakovic@gmail.com>
2019-11-30 20:24:06 +0000

    Adding biometric management api post directory and generating static files

---
+++ src/routes/posts/2019-11-30-android_biometric_mangement_apis/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Android Biometric Management APIs`,
+  tags: ['Mobile', 'Android', 'Biometrics'],
+  spoiler: "A basic overview of the new biometric management APIs introduced in Android 10. This showcases a basic example, talks about the potential reasoning behind the shift, and some of the inherent risks introduced as a result.",
+  getContent: () => import('./document.mdx'),
+}
