31359efdf65332143dc2b02c9972d3b0c073ded0
Nikola <nickcucakovic@gmail.com>
2019-11-30 20:23:23 +0000

    Updating r2con post to use capitalised tags

--- src/routes/posts/2019-09-10-r2con2019/post.js
+++ src/routes/posts/2019-09-10-r2con2019/post.js
@@ -1,6 +1,6 @@
 export default {
   title: `R2Con CTF - Land of Ecodelia`,
-  tags: ['reverse-engineering', 'write-up', 'ctf'],
+  tags: ['Reverse-Engineering', 'CTF'],
   spoiler: "A walk through on reversing a CTF challenge from R2con 2019. This breaks down the process of tackling an unknown binary written in Golang and show cases some of the capabilities of the Radare2 framework.",
   getContent: () => import('./document.mdx'),
 }
