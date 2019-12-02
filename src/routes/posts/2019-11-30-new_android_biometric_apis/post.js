b52c5f113ecf128b9ecd6d704276cd8e4bb56234
Nikola <nickcucakovic@gmail.com>
2019-12-02 01:27:15 +0000

    Updating title and further expanding on examples in post

--- src/routes/posts/2019-11-30-new_android_biometric_apis/post.js
+++ src/routes/posts/2019-11-30-new_android_biometric_apis/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `New Android Biometric APIs - Using Crypto Objects / Security Concerns`,
+  title: `Android Biometric APIs - Using Crypto Objects in Kotlin`,
   tags: ['Mobile', 'Android', 'Biometrics'],
   spoiler: "A basic overview of the new biometric APIs introduced in Android 10. This showcases a basic example using Crypto Objects, talk about the new biometric architecture, and some of the inherent risks introduced as a result.",
   getContent: () => import('./document.mdx'),
