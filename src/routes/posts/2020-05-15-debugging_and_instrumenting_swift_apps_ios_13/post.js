5e00253bf88b1b040e1ec300d05f9bb3d1afac90
arbitraryrw <nickcucakovic@gmail.com>
2020-05-16 13:07:52 +0100

    Adding placeholder page for new post with some references

---
+++ src/routes/posts/2020-05-15-debugging_and_instrumenting_swift_apps_ios_13/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: `Debugging & Instrumenting Swift Applications on iOS 13`,
+  tags: ['Mobile', 'iOS', 'Frida'],
+  spoiler: "A high level overview of debugging and dynamically instrumenting Swift mobile applications on iOS 13. This post goes into detail on some of the nuances of testing on iOS 13, some tips on working with Swift apps with the dynamic instrumentation framework Frida, and some general information for getting set up.",
+  getContent: () => import('./document.mdx'),
+}
+
