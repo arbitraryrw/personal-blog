cde5f9ef512d8e1da11c8fba7f8c5bee59dfc3dd
arbitraryrw <nickcucakovic@gmail.com>
2020-08-07 16:44:56 +0100

    Stealthy root post: removed boiler plate and added useful links

--- src/routes/posts/2020-08-09-custom_stealthy_android_root/post.js
+++ src/routes/posts/2020-08-09-custom_stealthy_android_root/post.js
@@ -1,7 +1,7 @@
 export default {
   title: `Custom Stealthy Android Root`,
   tags: ['Mobile', 'Android'],
-  spoiler: "Custom stealthy root walkthrough",
+  spoiler: "An introduction into how existing popular rooting frameworks can be customised to provide a more stealthy solution. This post highlights the importance of setting up a streamlined Android environment for security testing. In addition, I walk through an open source tool I wrote for modifying one of these frameworks that makes it virtually undetectable.",
   getContent: () => import('./document.mdx'),
 }
 
