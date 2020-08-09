93a7ac6c9541d51c612d00ec38d0040b8a57af74
arbitraryrw <nickcucakovic@gmail.com>
2020-08-09 18:02:22 +0100

    Stealthy root post: updating post title and expanding on content

--- src/routes/posts/2020-08-09-custom_stealthy_android_root/post.js
+++ src/routes/posts/2020-08-09-custom_stealthy_android_root/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Custom Stealthy Android Root`,
+  title: `Patching SuperSu to be Stealthy`,
   tags: ['Mobile', 'Android'],
   spoiler: "An introduction into how existing popular rooting frameworks can be customised to provide a more stealthy solution. This post highlights the importance of setting up a streamlined Android environment for security testing. In addition, I walk through an open source tool I wrote for modifying one of these frameworks that makes it virtually undetectable.",
   getContent: () => import('./document.mdx'),
