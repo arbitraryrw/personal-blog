2d93caed03baa7446088fb778cff8b06073dc38e
arbitraryrw <nickcucakovic@gmail.com>
2020-09-05 19:56:57 +0100

    Publishing r2con2020 blog post

---
+++ src/routes/posts/2020-09-05-r2con2020/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: `R2Con2020 CTF - Cyberlock`,
+  tags: ['Reverse-Engineering', 'CTF'],
+  spoiler: "A walk through of solving the Cyberlock CTF challenge from r2con2020 using the Radare2 framework. This post breaks down the process of reversing an unknown x86 binary and showcases some of the capabilities of the framework.",
+  getContent: () => import('./document.mdx'),
+}
+
