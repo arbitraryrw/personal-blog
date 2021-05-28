670f84620120d3444c928d09f75a1b1c47a0dd5d
arbitraryrw <nickcucakovic@gmail.com>
2021-05-28 23:47:43 +0100

    Cleaning up post preview for Handling Relative URLs to be more concise

--- src/routes/posts/2021-05-10-handling_relative_urls/post.js
+++ src/routes/posts/2021-05-10-handling_relative_urls/post.js
@@ -1,7 +1,7 @@
 export default {
-  title: `Handling Relative URLs for Redirect / Forwards`,
+  title: `Handling Relative URLs for Redirects / Forwards`,
   tags: ['Web'],
-  spoiler: "Remediating unvalidated forwards and redirects is almost always achieved by using an allow list of complete URLs. However, what do you do if the complete URL cannot be determined ahead of time? Unfortunately the default solution to the problem does not apply to every corner case. This post digs into validating URLs for redirects / forwards using a relative path.",
+  spoiler: "Remediating unvalidated forwards and redirects is almost always achieved by using an allow list of absolute URLs. Although what do you do if the absolute URL is not known ahead of time? This post digs into validating URLs for redirects / forwards using a relative path.",
   getContent: () => import('./document.mdx'),
 }
 
