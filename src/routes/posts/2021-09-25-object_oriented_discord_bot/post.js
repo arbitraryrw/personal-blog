d455562c1ac8c5da0bda67f3b0272a52ff5cb30c
arbitraryrw <nickcucakovic@gmail.com>
2021-09-25 11:11:43 +0100

    Creating boiler plate for discord bot (object orientated) post

---
+++ src/routes/posts/2021-09-25-object_oriented_discord_bot/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: `Object Oriented Discord Bot in Python`,
+  tags: ['Development'],
+  spoiler: "Discord bots are a great way to enhance the functionality of a server and enrich user experience. There are many tutorials out there that provide a quick and dirty approach to creating a discord bot but very few, if any, discuss scalable alternatives.This post digs into creating and structuring a discord bot for larger projects using object orientated programming.",
+  getContent: () => import('./document.mdx'),
+}
+
